a1076483b82784c912d81e6f8aed4dd944b39724
Julien Wan <julien.wan@aldes.com>
2026-07-27 08:45:48 +0200

    dev: added Println function

--- general.go
+++ general.go
@@ -1,9 +1,19 @@
 package core
 
+import "fmt"
+
 // IfThenElse returns the valueIfTrue if the condition is true, otherwise it returns the valueIfFalse
 func IfThenElse[T any](condition bool, valueIfTrue, valueIfFalse T) T {
 	if condition {
 		return valueIfTrue
 	}
 	return valueIfFalse
+}
+
+// Println prints the given message to the console, formatting it with the given arguments if any
+func Println(msg string, args ...any) {
+	if len(args) > 0 {
+		msg = fmt.Sprintf(msg, args...)
+	}
+	fmt.Println(msg)
 }
